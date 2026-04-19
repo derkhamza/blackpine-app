062909720603499a06004897ea02cb7d47b85904
Blackpine Capital Advisory <contact@blackpinecap.com>
2026-04-19 09:36:07 +0100

    Fix metro config to use packages/engine instead of sibling path

--- metro.config.js
+++ metro.config.js
@@ -11,4 +11,17 @@ config.resolver.nodeModulesPaths = [
   path.resolve(enginePath, "node_modules"),
 ];
 
+module.exports = config;const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const config = getDefaultConfig(__dirname);
+
+const enginePath = path.resolve(__dirname, "packages", "engine");
+
+config.watchFolders = [enginePath];
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, "node_modules"),
+  path.resolve(enginePath, "node_modules"),
+];
+
 module.exports = config;
